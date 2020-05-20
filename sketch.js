84e81daa672ff5d1a90cd3c8591b3165c1244be2
AJITESH3210 <64904115+AJITESH3210@users.noreply.github.com>
2020-05-20 09:19:07 +0530

    GCSO FILES ADDED

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ function setup() {
   createCanvas(1600,400);
   car = createSprite(50, 200, 50, 50);
   wall = createSprite(1500,200,60,height/2);
-  wall.shapeColor(80,80,80);
+  wall.shapeColor = color(80,80,80);
 
   speed = random(59,90);
   weight = random(400,1500);
